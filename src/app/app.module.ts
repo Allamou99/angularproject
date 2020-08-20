4bd7260b5c258b1307c690429d51268fb932707b
Soufiane Allamou <soufianeallamou99@outlook.fr>
2020-08-20 21:19:17 +0100

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {MenuComponent } from './menu/menu.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import {DishService} from './services/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +28,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
