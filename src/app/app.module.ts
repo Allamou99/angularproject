1c74a25fc2cfa83971e3f671f0f8cfab7615c86c
Soufiane Allamou <soufianeallamou99@outlook.fr>
2020-08-25 10:14:56 +0100

    template-driven forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,10 @@ import {PromotionService} from './services/promotion.service';
 import{LeaderService} from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import {MatDialogModule} from '@angular/material/dialog';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -47,7 +50,11 @@ import {MatDialogModule} from '@angular/material/dialog';
     AppRoutingModule,
     MatButtonModule,
     MatCardModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [
     DishService,
